fab8f948faef31c2fcbf17ff6bd7440df3f0bb81
Rafael Rojas <rafaelrojas.cov@gmail.com>
2020-09-27 20:24:12 -0300

    Clean up the component with Apollo React Hooks

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,83 +1,32 @@
-import React, { useReducer, useEffect } from "react";
-import { addMessage, getMessages, onMessageAdded } from "./graphql/queries";
+import React, { useState } from "react";
+import { useQuery, useMutation, useSubscription } from "@apollo/react-hooks";
+import * as Queries from "./graphql/queries";
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
 
-const initialState = {
-  messages: [],
-  subscription: null,
-};
-
-const ActionTypes = {
-  setMessages: "setMessages",
-  setSubscription: "setSubscription",
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case ActionTypes.setMessages: {
-      const { messages } = action.payload;
-      return {
-        ...state,
-        messages: [...state.messages, ...messages],
-      };
-    }
-    case ActionTypes.setSubscription: {
-      const { subscription } = action.payload;
-      return {
-        ...state,
-        subscription,
-      };
-    }
-    default:
-      return state;
-  }
-};
-
-// action creators
-const setMessages = (messages) => (dispatch) => {
-  dispatch({
-    type: ActionTypes.setMessages,
-    payload: { messages },
-  });
-};
-
-const setSubscription = (subscription) => (dispatch) => {
-  dispatch({ type: ActionTypes.setSubscription, payload: { subscription } });
-};
-
 // component
 const Chat = ({ user }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const call = (action) => action(dispatch);
+  const [messages, setMessages] = useState([]);
+
+  useQuery(Queries.messagesQuery, {
+    onCompleted: ({ messages }) => setMessages(messages),
+  });
+
+  useSubscription(Queries.messageAddedSubscription, {
+    onSubscriptionData: ({
+      subscriptionData: {
+        data: { messageAdded },
+      },
+    }) => {
+      setMessages(messages.concat(messageAdded));
+    },
+  });
+
+  const [addMessage] = useMutation(Queries.addMessageMutation);
 
   const handleSend = async (text) => {
-    await addMessage(text);
+    await addMessage({ variables: { input: { text } } });
   };
-
-  useEffect(() => {
-    const { subscription } = state;
-
-    const initialize = async () => {
-      const data = await getMessages();
-      const subscribe = onMessageAdded((message) => {
-        call(setMessages([message]));
-      });
-
-      call(setMessages(data));
-      call(setSubscription(subscribe));
-    };
-
-    initialize();
-
-    return () => {
-      if (subscription) {
-        subscription.unsubscribe();
-      }
-    };
-  }, []);
-
-  const { messages } = state;
 
   return (
     <section className="section">
