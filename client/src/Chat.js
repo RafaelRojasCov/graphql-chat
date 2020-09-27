3d77db6cf8b2d032b9dfe97a6fa96dbc7c8c2c89
Rafael Rojas <rafaelrojas.cov@gmail.com>
2020-09-27 19:48:42 -0300

    transform from class to function Chat component

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,45 +1,93 @@
-import React, { Component } from "react";
+import React, { useReducer, useEffect } from "react";
 import { addMessage, getMessages, onMessageAdded } from "./graphql/queries";
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
 
-class Chat extends Component {
-  state = { messages: [] };
-  subscription = null;
+const initialState = {
+  messages: [],
+  subscription: null,
+};
 
-  async componentDidMount() {
-    const messages = await getMessages();
-    this.setState({ messages });
-    this.subscription = onMessageAdded((message) => {
-      this.setState({ messages: this.state.messages.concat(message) });
-    });
-  }
+const ActionTypes = {
+  setMessages: "setMessages",
+  setSubscription: "setSubscription",
+};
 
-  async handleSend(text) {
-    await addMessage(text);
-  }
-
-  componentWillUnmount() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ActionTypes.setMessages: {
+      const { messages } = action.payload;
+      return {
+        ...state,
+        messages: [...state.messages, ...messages],
+      };
     }
+    case ActionTypes.setSubscription: {
+      const { subscription } = action.payload;
+      return {
+        ...state,
+        subscription,
+      };
+    }
+    default:
+      return state;
   }
+};
 
-  component;
+// action creators
+const setMessages = (messages) => (dispatch) => {
+  dispatch({
+    type: ActionTypes.setMessages,
+    payload: { messages },
+  });
+};
 
-  render() {
-    const { user } = this.props;
-    const { messages } = this.state;
-    return (
-      <section className="section">
-        <div className="container">
-          <h1 className="title">Chatting as {user}</h1>
-          <MessageList user={user} messages={messages} />
-          <MessageInput onSend={this.handleSend.bind(this)} />
-        </div>
-      </section>
-    );
-  }
-}
+const setSubscription = (subscription) => (dispatch) => {
+  dispatch({ type: ActionTypes.setSubscription, payload: { subscription } });
+};
+
+// component
+const Chat = ({ user }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const call = (action) => action(dispatch);
+
+  const handleSend = async (text) => {
+    await addMessage(text);
+  };
+
+  useEffect(() => {
+    const { subscription } = state;
+
+    const initialize = async () => {
+      const data = await getMessages();
+      const subscribe = onMessageAdded((message) => {
+        call(setMessages([message]));
+      });
+
+      call(setMessages(data));
+      call(setSubscription(subscribe));
+    };
+
+    initialize();
+
+    return () => {
+      if (subscription) {
+        subscription.unsubscribe();
+      }
+    };
+  }, []);
+
+  const { messages } = state;
+
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Chatting as {user}</h1>
+        <MessageList user={user} messages={messages} />
+        <MessageInput onSend={handleSend} />
+      </div>
+    </section>
+  );
+};
 
 export default Chat;
